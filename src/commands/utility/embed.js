b8aaa37725bd277212df85bba3c0a0dcf3119c7f
Ilias Kourousis <i.k.kourousis@gmail.com>
2023-12-12 00:14:18 +0000

    embed format

--- src/commands/utility/embed.js
+++ src/commands/utility/embed.js
@@ -1,61 +1,75 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
+
+const exampleEmbed = {
+  color: 0x0099ff,
+  title: "GHR Hotstint",
+  description: "Track - Spa",
+  fields: [
+    {
+      name: "Regular field title",
+      value: "Some value here",
+    },
+    {
+      name: "\u200b",
+      value: "\u200b",
+      inline: false,
+    },
+    {
+      name: "# Gamertag",
+      value: "1. Washamga",
+      inline: true,
+    },
+    {
+      name: "Car",
+      value: "McLaren 720s EVO",
+      inline: true,
+    },
+    {
+      name: "Time".padEnd(15) + "Gap",
+      value: "2:17.335",
+      inline: true,
+    },
+    {
+      name: "",
+      value: "2. FlyingJim8",
+      inline: true,
+    },
+    {
+      name: "",
+      value: "McLaren 720s EVO",
+      inline: true,
+    },
+    {
+      name: "",
+      value: "2:18.335 | +1.000",
+      inline: true,
+    },
+    {
+      name: "",
+      value: "3. EpicGT",
+      inline: true,
+    },
+    {
+      name: "",
+      value: "Ferrari 296 GT3",
+      inline: true,
+    },
+    {
+      name: "",
+      value: "2:19.835 | +2.500",
+      inline: true,
+    },
+  ],
+};
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('embedsend')
-		.setDescription('Test to send an embed'),
-	async execute(interaction) {
-        const exampleEmbed = {
-            color: 0x0099ff,
-            title: 'Some title',
-            url: 'https://discord.js.org',
-            author: {
-                name: 'Some name',
-                icon_url: 'https://i.imgur.com/AfFp7pu.png',
-                url: 'https://discord.js.org',
-            },
-            description: 'Some description here',
-            thumbnail: {
-                url: 'https://i.imgur.com/AfFp7pu.png',
-            },
-            fields: [
-                {
-                    name: 'Regular field title',
-                    value: 'Some value here',
-                },
-                {
-                    name: '\u200b',
-                    value: '\u200b',
-                    inline: false,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
-            ],
-            image: {
-                url: 'https://i.imgur.com/AfFp7pu.png',
-            },
-            timestamp: new Date().toISOString(),
-            footer: {
-                text: 'Some footer text here',
-                icon_url: 'https://i.imgur.com/AfFp7pu.png',
-            },
-        };
-		// interaction.user is the object representing the User who ran the command
-		// interaction.member is the GuildMember object, which represents the user in the specific guild
-        
-        await interaction.send({ embeds: [exampleEmbed] });
-	},
+  category: "utility",
+  data: new SlashCommandBuilder()
+    .setName("embed")
+    .setDescription("Posts or edits a simple embed"),
+  async execute(interaction) {
+    // If a message has already been sent, edit the existing message with a new embed
+    await interaction.reply({ embeds: [exampleEmbed] });
+  },
 };
